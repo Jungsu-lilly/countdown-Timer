21f4741cb7ed8938ccff7cbcb917045ac8f2ecbf
matt1235 <matt1235@naver.com>
2021-10-20 21:22:52 +0900

    카운트다운 기능 구현

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
 const newYears = "1 Jan 2022";
 
 function countdown(){
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
-    const seconds = (newYearsDate -currentDate) / 1000;
-    const days = Math.floor(seconds / 3600 / 24);
-    console.log(days);
+    const Totalseconds = (newYearsDate -currentDate) / 1000;
+    const days = Math.floor(Totalseconds / 3600 / 24);
+    const hours = Math.floor(Totalseconds / 3600) % 24;
+    const mins = Math.floor(Totalseconds / 60) % 60;
+    const seconds = Math.floor(Totalseconds)% 60;
+
+    console.log(mins);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
 }
 
 // initial call
